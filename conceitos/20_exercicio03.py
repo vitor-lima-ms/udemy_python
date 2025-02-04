6c948af99f6aad4bd673b0585396bb0732faaabb
Vitor Lima <viitorlima2000@gmail.com>
2025-02-04 19:01:53 -0300

    Conceitos

--- conceitos/20_exercicio03.py
+++ conceitos/20_exercicio03.py
@@ -1,7 +1,7 @@
 '''Programa que pede o primeiro nome do usuario --> Se o nome tiver 4 letras ou menos == "Seu nome e curto" --> Se o nome tiver entre 5 e 6 letras == "Seu nome é normal" --> Se o nome tiver mais que 6 letras == "Seu nome e muito grande"'''
 
 nome = input('Digite o seu nome: ')
-while (nome.isnumeric() is True) or (nome.isalnum() is False):
+while (nome.isnumeric() is True) or (nome.isalnum() is False) or (nome.isalpha() is False) or (len(nome) < 3):
     print('Nome inválido!')
     nome = input('Por favor, digite o seu nome: ')
 
