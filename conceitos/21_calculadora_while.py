37d5b795d1dc88197fbf07a42bf06b54b339ef87
Vitor Lima <viitorlima2000@gmail.com>
2025-02-04 20:41:47 -0300

    Update 21_calculadora_while.py

--- conceitos/21_calculadora_while.py
+++ conceitos/21_calculadora_while.py
@@ -1,79 +1,63 @@
-contador = 0
-
 while True:
-
-    contador += 1
-
-    if contador == 1:
         
-        num1_str = input('Digite um número: ')
+    num1_str = input('Digite um número: ')
         
-        while (num1_str.replace('.', '').replace(',', '').isnumeric() is False):
+    while (num1_str.replace('.', '').replace(',', '').isnumeric() is False):
             
-            print('Isso não é um número!')
+        print('Isso não é um número!')
 
-            num1_str = input('Digite um número: ')
+        num1_str = input('Digite um número: ')
     
-    elif contador == 2:
+    num2_str = input('Digite outro número: ')
+        
+    while (num2_str.replace('.', '').replace(',', '').isnumeric() is False):
+            
+        print('Isso não é um número!')
 
         num2_str = input('Digite outro número: ')
-        
-        while (num2_str.replace('.', '').replace(',', '').isnumeric() is False):
-            
-            print('Isso não é um número!')
-
-            num2_str = input('Digite outro número: ')
     
-    elif contador == 3:
+    operador = input('Digite um operador [+, -, * ou /]: ')
+
+    while (operador not in '+-*/') or (len(operador) > 1):
+
+        print('Esse não é um operador válido!')
 
         operador = input('Digite um operador [+, -, * ou /]: ')
-
-        while operador not in '+-*/':
-
-            print('Esse não é um operador válido!')
-
-            operador = input('Digite um operador [+, -, * ou /]: ')
     
-    elif contador == 4:
-
-        if operador == '+':
+    if operador == '+':
     
-            resultado = float(num1_str) + float(num2_str)
+        resultado = float(num1_str.replace(',', '.')) + float(num2_str.replace(',', '.'))
 
-            print(f'O resultado da operação é: {resultado}!')
+        print(f'O resultado da operação é: {resultado}!')
 
-        elif operador == '-':
+    elif operador == '-':
 
-            resultado = float(num1_str) - float(num2_str)
+        resultado = float(num1_str.replace(',', '.')) - float(num2_str.replace(',', '.'))
 
-            print(f'O resultado da operação é: {resultado}!')
+        print(f'O resultado da operação é: {resultado}!')
 
-        elif operador == '*':
+    elif operador == '*':
 
-            resultado = float(num1_str) * float(num2_str)
+        resultado = float(num1_str.replace(',', '.')) * float(num2_str.replace(',', '.'))
             
-            print(f'O resultado da operação é: {resultado}!')
+        print(f'O resultado da operação é: {resultado}!')
 
-        else:
+    else:
 
-            resultado = float(num1_str) / float (num2_str)
+        resultado = float(num1_str.replace(',', '.')) / float (num2_str.replace(',', '.'))
 
-            print(f'O resultado da operação é: {resultado}!')
+        print(f'O resultado da operação é: {resultado:.2f}!')
+
+    continuar = input('Deseja realizar mais alguma operação [S/N]? ')
+
+    while (continuar not in 'Nn') and (continuar not in 'Ss'):
+
+        print('Opção inválida!')
 
         continuar = input('Deseja realizar mais alguma operação [S/N]? ')
-
-        while (continuar not in 'Nn') and (continuar not in 'Ss'):
-
-            print('Opção inválida!')
-
-            continuar = input('Deseja realizar mais alguma operação [S/N]? ')
         
-        if continuar in 'Nn':
+    if continuar in 'Nn':
 
-            break
-
-        else:
-
-            contador = 0
+        break
 
 print('Volte sempre!')
