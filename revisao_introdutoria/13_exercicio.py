cc1b80e0101af37bc21e132ef6a7c433f826dc35
Vitor Lima <viitorlima2000@gmail.com>
2025-02-04 16:37:24 -0300

    Conteúdo de revisão

---
+++ revisao_introdutoria/13_exercicio.py
@@ -0,0 +1,6 @@
+primeiro_valor = input('Digite o primeiro valor: ')
+segundo_valor = input('Digite o segundo valor: ')
+if primeiro_valor >= segundo_valor:
+    print(f'O primeiro valor {primeiro_valor} é maior ou igual que o segundo valor {segundo_valor}')
+else:
+    print(f'O segundo valor {segundo_valor} é maior que o primeiro valor {primeiro_valor}')
