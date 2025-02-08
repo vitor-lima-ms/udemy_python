57d892b66ba57d95aa85dca0bac1d5a1a1849e2c
Vitor Lima <viitorlima2000@gmail.com>
2025-02-07 21:38:05 -0300

    poo

--- poo/20_heranca.py
+++ poo/20_heranca.py
@@ -1,1 +1,29 @@
-#
+#Heranca simples --> Relacao entre classes
+
+#Classe principal (e.g., Pessoa) --> super class, base class, parent class
+    #Classes filhas (e.g., Cliente) --> sub class, child class, derived class
+    #A classe Pessoa pode ser enxergada como uma generalizacao da classe Cliente --> Cliente e uma Pessoa
+    #Poderiamos ter feito o processo contrario e especializado Pessoa em Cliente --> Especializacao
+#Em Python, todas as classe ja herdam de um builtin --> Coisas que ja vem por padrao no Python
+
+class Person:
+    def __init__(self, name, last_name):
+        self.name = name
+        self.last_name = last_name
+    
+    def speak_class_name(self):
+        print(self.name, self.last_name, self.__class__.__name__) #Exibir o nome da classe que esta chamando o metodo
+
+class Client(Person): #Client herda de Person. Todo codigo que esta dentro de Person e automaticamente usado por Client
+    ...
+
+class Student(Person):
+    ...
+
+c1 = Client('Vitor', 'Silva')
+s1 = Student('Maria', 'Silva')
+
+c1.speak_class_name() # --> class Client
+s1.speak_class_name() # --> class Student
+
+#Method resolution order (MRO) --> Ordem que o Python para procurar metodos e atributos --> Usar help(Classe) para ver a MRO
---
+++ poo/21_sobreposicao_metodos.py
@@ -0,0 +1,30 @@
+'''class MyString (str): #Herdou todos os metodos da classe str
+    def upper(self):
+        print('Meu .upper()')
+        result = super().upper() #Poderia fazer dessa forma tb caso quisesse realizar alguma acao depois de retornar o metodo da superclasse
+        return super().upper() #Permite que eu define alguma coisa no escopo do meu metodo sem inutilizar o metodo da superclasse
+
+my_string = MyString('Vitor')
+print(my_string.upper()) #Se eu definir um metodo upper na MyString, vou estragar o metodo upper da classe str --> MRO comeca na classe filha --> Contudo, podemos usar a classe super() dentro do metodo para que o metodo da superclasse seja utilizado'''
+
+class A:
+    attr_a = 'A attr'
+    def method(self):
+        print('A')
+
+class B(A):
+    attr_b = 'B attr'
+    def method(self):
+        print('B')
+
+class C(B):
+    attr_c = 'C attr'
+    def method(self):
+        super(C, self).method() #A partir dessa classe, procure outra classe com o metodo method que, nesse caso, e B. Poderiamos passar B como argumento, entao o metodo seria buscado em A
+        print('C')
+
+c = C()
+print(c.attr_a)
+print(c.attr_b)
+print(c.attr_c) #C herdou tudo de B que herdou de A. Apenas o metodo que nao, pois ele tem o mesmo nome nas 3, logo o MRO so utilizara o de C
+print(C.mro())
